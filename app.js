5c0f1dba28e5aba59ad5dd088a2337501afa9112
EliDev <eliasss.mb@gmail.com>
2024-07-07 14:55:58 -0300

    deploy

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express, { json } from 'express'
 import { createGameRouter } from './routes/games-routes.js'
 import { corsMiddleware } from './middlewares/cors.js'
+import 'dotenv/config'
 
 export const createApp = ({ gameModel }) => {
   const app = express()
